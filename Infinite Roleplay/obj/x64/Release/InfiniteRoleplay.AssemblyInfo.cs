1036f8e511b71986d768a8a2764e1ae200b7b9a9
serifas <preachandteach@outlook.com>
2024-03-21 05:41:17 -0700

    d

--- Infinite Roleplay/obj/x64/Release/InfiniteRoleplay.AssemblyInfo.cs
+++ Infinite Roleplay/obj/x64/Release/InfiniteRoleplay.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("An all around Roleplay Plugin.")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("0.0.0.1")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.0.0.1+d2d9a93a25284f1f14abd5e84588219b6669d20c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.0.0.1+ae24e97af66ce76145819e8a5a5e39e864b11896")]
 [assembly: System.Reflection.AssemblyProductAttribute("InfiniteRoleplay")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InfiniteRoleplay")]
 [assembly: System.Reflection.AssemblyVersionAttribute("0.0.0.1")]
